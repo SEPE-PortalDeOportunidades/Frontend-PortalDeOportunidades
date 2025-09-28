b9dd4515dbeff1897e2f46117cc9a4bf153767af
João Pedro <joaops41224@gmail.com>
2025-09-28 16:55:44 -0300

    FEAT: create login page and global components

---
+++ src/components/Global/GlobalButton.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { defineProps, defineEmits } from "vue"
+
+// Props do botão
+defineProps({
+  label: { type: String, required: true },
+  type: { type: String, default: "button" },
+  disabled: { type: Boolean, default: false }
+})
+
+const emit = defineEmits(["click"])
+</script>
+
+<template>
+  <button
+    :type="type"
+    :disabled="disabled"
+    @click="emit('click')"
+    class="globalbutton"
+
+  >
+    {{ label }}
+    
+  </button>
+</template>
+
+<style scoped>
+.globalbutton {
+  padding: 0.5rem 1rem; 
+  background-color: #000000; 
+  color: white; 
+  border-radius: 0.375rem; 
+  transition: background-color 0.3s ease;
+  cursor: pointer;
+  width: 200px;
+  height: 40px;
+  font-size: 18px;
+}
+
+.globalbutton:hover {
+  background-color: #3d3d3d; 
+}
+
+
+</style>
---
+++ src/components/Global/GlobalInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps, defineEmits } from "vue"
+defineProps ({
+    modelValue: String,  //modelvalue é tipo o treinador entregando a bola pro jogador treinar(inital value)
+    label: String,  //tipo da label, no caso, vai ser uma string
+    type: {
+        type: String, 
+        default: "text" //se o tecnico nao fala nada, o jogador entra como armador, no caso text = armador
+    }
+
+    
+})
+
+const emit = defineEmits(["update:modelValue"]) 
+</script>
+<template>
+  <div>
+    <label v-if="label" :for="label" class="block mb-1">{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :type="type"
+      :value="modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+
+    />
+  </div>
+
+</template>
+
+<style>
+input {
+    border-radius: 15px;
+    width: 375px;
+    height: 40px;
+    background-color: rgb(247, 247, 247);
+    border: none;
+    margin-bottom: 15px;
+}
+</style>
