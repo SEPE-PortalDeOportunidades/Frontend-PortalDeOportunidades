54b73ce533bf5a18efe71e0d1cbed255709c8b35
João Pedro <joaops41224@gmail.com>
2025-10-01 23:09:24 -0300

    FEAT: create login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,74 +1,67 @@
-<script setup>
-import GlobalInput from "@/components/Global/GlobalInput.vue";
-import GlobalButton from "@/components/Global/GlobalButton.vue";
-import { ref } from "vue";
+<script>
 
+import GlobalInput from "@/components/Global/GlobalInput.vue"
+import GlobalButton from "@/components/Global/GlobalButton.vue"
+import { ref } from "vue"
 
-const matricula = ref("");
-const senha = ref("");
-
-const dados = ref([]);
-
-const enviarFormulario = () => {
-  dados.value.push({
-    matricula: matricula.value,
-    senha: senha.value,
-  });
-  
+const email = ref("")
+const senha = ref("")
 </script>
+
+
 <template>
+    <div class="background">
+        <div class="container">
+
             <form @submit.prevent="handleLogin">
                 <div>
                     <h1>Portal de Oportunidades</h1>
                     <label for="GlobalInput">Matricula*</label>
                     <GlobalInput v-model="email" placeholder="" type="email" class="mb-4 input" />
 
-  <div class="background">
-    <div class="container">
-      <form @submit.prevent="handleLogin">
-        <div>
-          <h1>Portal de Oportunidades</h1>
-          <label for="GlobalInput">Matricula</label>
-          <GlobalInput
-            v-model="matricula"
-            placeholder="Digite sua matrícula"
-            type="text"
-            class="mb-4 input"
-          />
+
+                </div>
+                <div>
+                    <label for="GlobalInput">Senha*</label>
+                    <GlobalInput v-model="senha" placeholder="" type="password" class="mb-4 input" />
+                </div>
+                <p>Esqueceu a senha? <router-link to="/forgot-password">Clique Aqui</router-link></p>
+                <GlobalButton label="Entrar" type="submit" @click="enviarFormulario" />
+            </form>
+            <p class="ifc">Copyright © {{ new Date().getFullYear() }} <br>IFC - Campus Araquari</p>
+
         </div>
-        <div>
-          <label for="GlobalInput">Senha</label>
-          <GlobalInput
-            v-model="senha"
-            placeholder="Digite sua senha"
-            type="password"
-            class="mb-4 input"
-          />
-        </div>
-        <p>Esqueceu a senha? <a href="#">Clique Aqui</a></p>
-        <GlobalButton label="Entrar" type="submit" @click="enviarFormulario" />
-      </form>
-      <p class="ifc">Copyright © 2025 <br />IFC - Campus Araquari</p>
     </div>
-  </div>
 </template>
 
 <style scoped>
+:global(html, body) {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden; 
+}
+
 .background {
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;  
   height: 100vh;
   overflow: hidden;
 }
 
 .background::before {
-  content: "";
-  position: absolute;
-  inset: 0;
-  background-image: url("@/assets/images/background-login.png");
-  background-size: cover;
-  background-position: center;
-  filter: blur(3px);
-  z-index: 0;
+    content: "";
+    position: absolute;
+    inset: 0;
+    background-image: url('@/assets/images/background-login.png');
+    background-size: cover;
+    background-position: center;
+    filter: blur(3px);
+    z-index: 0;
+    overflow: hidden;
+
 }
 
 .container {
@@ -84,31 +77,36 @@ const enviarFormulario = () => {
 }
 
 h1 {
-  position: relative;
-  bottom: 30px;
+    position: relative;
+    bottom: 30px;
 }
 
 label {
-  margin: 5px;
+    margin: 5px;
+
+
 }
+
 
 p {
     position: relative;
     bottom: 20px;
+
 }
 
 button {
-  position: relative;
-  left: 15%;
-  top: 25px;
+    position: relative;
+    left: 15%;
+    top: 25px;
+
+
 }
 
 .ifc {
-  display: flex;
-  justify-content: center;
-  right: 45px;
-  top: 50px;
-  text-align: center;
+    display: flex;
+    justify-content: center;
+    right: 45px;
+    top: 50px;
+    text-align: center;
 }
-
 </style>
