41b03be912522c6da7b565503a362db6e5a93178
João Pedro <joaops41224@gmail.com>
2025-10-04 23:50:39 -0300

    FEAT: create eventCard

---
+++ src/teste.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+import EventCards from "@/components/EventCards.vue";
+</script>
+<template>
+    <EventCards>
+        <template #tipo>Estágio</template>
+        <template #titulo>Estágio em Desenvolvimento de Software</template>
+        <template #descricao>Oportunidade de estágio em empresa parceira para estudantes de Informática.</template>
+        <template #data>Prazo: 17/09/2025</template>
+        <template #professor>Por:Prof. Fabio Longo de Moura</template>
+        <template #curso> Informatica</template>
+        <template #requisitos>cursando a partir do 3º período, conhecimentos em programação</template>
+    </EventCards>
+</template>
+<style scoped>
+</style>
