1a197197c2d1d7c34cdded36faf62cbc62c91a6a
João Pedro <joaops41224@gmail.com>
2025-10-05 14:02:43 -0300

    FEAT: create teacher navegation

---
+++ src/components/teacherNav.vue
@@ -0,0 +1,61 @@
+<script setup>
+</script>
+<template>
+    <header>
+        <div class="img">
+        <img src="/src/assets/images/logoIFC.png" alt="LOGO IFC ARaQUARI">
+        </div>
+        <div class="infos">
+        <button>Nova oportunidade</button>
+        <font-awesome-icon class="icon" :icon="['fas', 'bell']" />
+        <font-awesome-icon class="icon" :icon="['fas', 'user']" />
+
+        </div>
+    </header>
+
+</template>
+<style scoped>
+header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 10px 0 20px;
+    border-bottom: 1px solid #000;
+    
+    
+}
+
+
+.infos {
+    padding-right: 15px;
+    display: flex;
+    align-items: center;
+}
+
+button {
+    background-color: black;
+    color: white;
+    width: 150px;
+    border-radius: 8px;
+    padding: 10px 0px;
+    margin-right: 25px;
+    transition: 0.2s ease;
+}
+button:hover {
+  transform: translateY(-2px);
+}
+
+.icon {
+    border: 1px solid black;
+    padding: 11px;
+    border-radius: 8px;
+    margin-right: 10px;
+      transition: 0.2s;
+          box-shadow: 0px 2px 2px gray;
+}
+
+.icon:hover{
+    box-shadow: 0px 2px 2px gray;
+    transform: translateY(-2px);
+}
+</style>
--- src/teste.vue
+++ src/teste.vue
@@ -1,17 +1,12 @@
 <script setup>
 import { ref } from "vue";
-import EventCards from "@/components/EventCards.vue";
+import teacherNav from "@/components/teacherNav.vue";
 </script>
 <template>
-    <EventCards>
-        <template #tipo>Estágio</template>
-        <template #titulo>Estágio em Desenvolvimento de Software</template>
-        <template #descricao>Oportunidade de estágio em empresa parceira para estudantes de Informática.</template>
-        <template #data>Prazo: 17/09/2025</template>
-        <template #professor>Por:Prof. Fabio Longo de Moura</template>
-        <template #curso> Informatica</template>
-        <template #requisitos>cursando a partir do 3º período, conhecimentos em programação</template>
-    </EventCards>
+    <teacherNav>
+        
+    </teacherNav>
 </template>
+
 <style scoped>
 </style>
